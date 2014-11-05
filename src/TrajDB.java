cf26ec406c9c3e30995a6f27d38b8a768e665763
Jonathan Huang <jon.hc.huang@gmail.com>
2014-11-04 22:02:59 -0500

    Added regex detection for different queries.

--- src/TrajDB.java
+++ src/TrajDB.java
@@ -1,34 +1,52 @@
+import jdk.nashorn.internal.runtime.regexp.joni.Syntax;
+import sun.tools.java.SyntaxError;
+
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by jhh11 on 11/4/14.
  */
 public class TrajDB {
-    public static void main (String[] args) {
-//        something to keep track if "EXIT" has been called
-//        while something is false
-        while (true) {
-            Scanner scan = new Scanner(System.in).useDelimiter(";");
-//        parse scan for regex;
-            System.out.print(">> ");
-            while(scan.hasNext()) {
-                scan.next().split
+    public static void main (String[] args) throws IOException {
+        boolean repl = true;
+        while (repl) {
+            try {
+                Scanner scan = new Scanner(System.in).useDelimiter(";");
+                System.out.print("TrajDB > ");
+                String input = scan.next();
+                input = input.replaceAll("\\s+"," ");
+                String[] argu = input.split(" ");
+                // parse regex
+                if (input.matches("^(CREATE \\w+)$")) {
+                    System.out.println("create");
+                    System.out.println("args: " + argu[1]);
+                } else if (input.matches("^(INSERT INTO \\w+ VALUES (([\\d\\.]+,[\\d\\.]+,[\\d\\.]+,[\\d\\.]+," +
+                        "[\\d\\.]+,[\\d\\-]+,[\\d\\:]+)\\s?)+)$")) {
+                    System.out.println("insert");
+                    String print = "args: " + argu[2];
+                    for(int i = 4; i < argu.length; i++) {
+                        print = print + ", " + argu[i];
+                    }
+                    System.out.println(print);
+                } else if (input.matches("^(DELETE FROM \\w+ TRAJECTORY \\d+)$")) {
+                    System.out.println("delete");
+                    System.out.println("args: " + argu[2] + ", " + argu[4]);
+                } else if (input.matches("^(RETRIEVE FROM \\w+ TRAJECTORY \\d+)$")) {
+                    System.out.println("retrieve trajectory set");
+                    System.out.println("args: " + argu[2] + ", " + argu[4]);
+                } else if (input.matches("^(RETRIEVE FROM \\w+ COUNT OF \\d+)$")) {
+                    System.out.println("retrieve trajectory count");
+                    System.out.println("args: " + argu[2] + ", " + argu[5]);
+                } else if (input.matches("^(EXIT)$")) {
+                    System.out.println("exit");
+                    repl = false;
+                } else {
+                    throw new SyntaxError();
+                }
             }
-            scan.close();gi
+            catch (Exception e) {e.printStackTrace();}
         }
-
-
-        // Initialize Scanner object
-        Scanner scan = new Scanner("Anna Mills/Female/18");
-        // initialize the string delimiter
-        scan.useDelimiter("/");
-        // Printing the delimiter used
-        System.out.println("The delimiter use is "+scan.delimiter());
-        // Printing the tokenized Strings
-        while(scan.hasNext()){
-            System.out.println(scan.next());
-        }
-        // closing the scanner stream
-        scan.close();
     }
 }
