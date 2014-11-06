68e64afa75b08147d552bb3684043b286b6deed1
alapparikh <alap18@gmail.com>
2014-11-06 02:25:38 -0500

    retrieve methods

---
+++ src/com/methods/RetrieveTraj.java
@@ -0,0 +1,13 @@
+package com.methods;
+
+/**
+ * Created by Alap on 11/5/14.
+ */
+public class RetrieveTraj {
+
+    public static String retrieve (String tname, int tid){
+        String trajectory = "";
+
+        return trajectory;
+    }
+}
